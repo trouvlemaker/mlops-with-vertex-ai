026d65b740df8803a05e6ab633c884f3a6ed1bae
trouvlemaker <66418112+trouvlemaker@users.noreply.github.com>
2022-07-05 17:59:02 +0900

    Update gcs-bucket.tf

--- provision/terraform/gcs-bucket.tf
+++ provision/terraform/gcs-bucket.tf
@@ -16,7 +16,8 @@
 
 resource "google_storage_bucket" "artifact_repo" {
     project       = module.project-services.project_id
-    name          = "${var.name_prefix}-bucket"
+    #name          = "${var.name_prefix}-bucket"
+    name          = "mlops-kaflix"
     location      = local.region
     storage_class = local.bucket_type
     force_destroy = var.force_destroy
